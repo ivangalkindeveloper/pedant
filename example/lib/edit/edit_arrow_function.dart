d5060f089ea24a26b00bffc4c4a54fdee8eb0a38
Ivan Galkin <ivangalkindeveloper@gmail.com>
2024-06-16 01:47:19 +0300

    Updating

--- example/lib/edit/edit_arrow_function.dart
+++ example/lib/edit/edit_arrow_function.dart
@@ -1,20 +1,21 @@
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 
-class SomeBloc extends Bloc<String, String> {
-  SomeBloc()
-      : super(
-          "",
-        ) {
-    on<String>(
-      (
-        String event,
-        Emitter<String> emit,
-      ) {},
-    );
-  }
-}
+// class SomeBloc extends Bloc<String, String> {
+//   SomeBloc()
+//       : super(
+//           "",
+//         ) {
+//     on<String>(
+//       (
+//         String event,
+//         Emitter<String> emit,
+//       ) {},
+//     );
+//   }
+// }
 
-void sort({
-  required List<String> strings,
-}) =>
-    strings.sort();
+// void sort({
+//   required List<String> strings,
+// }) {
+//   return strings.sort();
+// }
